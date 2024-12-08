4ec48725be2e28e06f098fc29f2779c9c00248ac
CHEELEONG_KHONG <khongcl1307@gmail.com>
2024-12-08 17:24:26 +0800

    added first name and last name to user entity

--- techfit.sql
+++ techfit.sql
@@ -5,6 +5,8 @@ USE techfit;
 CREATE TABLE User (
     user_id VARCHAR(5) PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     birthday DATE,
